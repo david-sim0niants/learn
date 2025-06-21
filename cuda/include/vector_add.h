c34094a4d9d6d3673bb03423fdd68ee52215078a
david-sim0niants <david.simonyants@gmail.com>
2025-06-22 00:59:21 +0400

    CUDA learn turned into submodule

--- cuda/include/vector_add.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cstddef>
-
-void add_vectors(const int *dev_a, const int *dev_b, int *dev_c, size_t size);
-void add_vectors(const float *dev_a, const float *dev_b, float *dev_c, size_t size);
-void add_vectors(const double *dev_a, const double *dev_b, double *dev_c, size_t size);
